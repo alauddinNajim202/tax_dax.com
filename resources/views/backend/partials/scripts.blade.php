e04151f7585a13f4678ea208cef5dcf7e6c64357
alauddin_najim <alauddinnajim202@gmail.com>
2024-10-12 14:50:22 +0600

    admin dashboard setting system implement

--- resources/views/backend/partials/scripts.blade.php
+++ resources/views/backend/partials/scripts.blade.php
@@ -1,50 +1,50 @@
 {{-- Axios.js --}}
-<script src="{{ asset('backend/custom_downloaded_file/axios.min.js') }}"></script>
+<script src="{{ asset('backend/assets/custom_downloaded_file/axios.min.js') }}"></script>
 
 {{-- moment.min.js --}}
-<script src="{{ asset('backend/custom_downloaded_file/moment.min.js') }}"></script>
+<script src="{{ asset('backend/assets/custom_downloaded_file/moment.min.js') }}"></script>
 
 {{-- JQUERY JS --}}
-<script src="{{ asset('backend/plugins/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('backend/assets/plugins/jquery/jquery.min.js') }}"></script>
 
 {{-- BOOTSTRAP JS --}}
-<script src="{{ asset('backend/plugins/bootstrap/js/popper.min.js') }}"></script>
-<script src="{{ asset('backend/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('backend/assets/plugins/bootstrap/js/popper.min.js') }}"></script>
+<script src="{{ asset('backend/assets/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
 
 {{-- SIDE-MENU JS --}}
-<script src="{{ asset('backend/plugins/sidemenu/sidemenu.js') }}"></script>
+<script src="{{ asset('backend/assets/plugins/sidemenu/sidemenu.js') }}"></script>
 
 {{-- Perfect SCROLLBAR JS --}}
-<script src="{{ asset('backend/plugins/p-scroll/perfect-scrollbar.js') }}"></script>
-<script src="{{ asset('backend/plugins/p-scroll/pscroll.js') }}"></script>
+<script src="{{ asset('backend/assets/plugins/p-scroll/perfect-scrollbar.js') }}"></script>
+<script src="{{ asset('backend/assets/plugins/p-scroll/pscroll.js') }}"></script>
 
 {{-- STICKY JS --}}
 <script src="{{ asset('backend/js/sticky.js') }}"></script>
 
 {{-- INTERNAL Summernote Editor js --}}
-<script src="{{ asset('backend/plugins/summernote-editor/summernote1.js') }}"></script>
-<script src="{{ asset('backend/js/summernote.js') }}"></script>
+<script src="{{ asset('backend/assets/plugins/summernote-editor/summernote1.js') }}"></script>
+<script src="{{ asset('backend/assets/js/summernote.js') }}"></script>
 
 {{-- dropify js --}}
-<script src="{{ asset('backend/js/dropify.min.js') }}"></script>
+<script src="{{ asset('backend/assets/js/dropify.min.js') }}"></script>
 
 {{-- toaster js --}}
-<script src="{{ asset('backend/js/toastr.min.js') }}"></script>
+<script src="{{ asset('backend/assets/js/toastr.min.js') }}"></script>
 
 {{-- DATA TABLE JS --}}
-<script src="{{ asset('backend/plugins/datatable/js/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('backend/plugins/datatable/js/dataTables.bootstrap5.js') }}"></script>
-<script src="{{ asset('backend/plugins/datatable/js/dataTables.buttons.min.js') }}"></script>
-<script src="{{ asset('backend/plugins/datatable/js/butsns.bootstrap5.min.js') }}"></script>
-<script src="{{ asset('backend/plugins/datatable/js/jszip.min.js') }}"></script>
-<script src="{{ asset('backend/plugins/datatable/pdfmake/pdfmake.min.js') }}"></script>
-<script src="{{ asset('backend/plugins/datatable/pdfmake/vfs_fonts.js') }}"></script>
-<script src="{{ asset('backend/plugins/datatable/js/butsns.html5.min.js') }}"></script>
-<script src="{{ asset('backend/plugins/datatable/js/buttons.print.min.js') }}"></script>
-<script src="{{ asset('backend/plugins/datatable/js/buttons.colVis.min.js') }}"></script>
-<script src="{{ asset('backend/plugins/datatable/dataTables.responsive.min.js') }}"></script>
-<script src="{{ asset('backend/plugins/datatable/responsive.bootstrap5.min.js') }}"></script>
-<script src="{{ asset('backend/js/table-data.js') }}"></script>
+<script src="{{ asset('backend/assets/plugins/datatable/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('backend/assets/plugins/datatable/js/dataTables.bootstrap5.js') }}"></script>
+<script src="{{ asset('backend/assets/plugins/datatable/js/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset('backend/assets/plugins/datatable/js/butsns.bootstrap5.min.js') }}"></script>
+<script src="{{ asset('backend/assets/plugins/datatable/js/jszip.min.js') }}"></script>
+<script src="{{ asset('backend/assets/plugins/datatable/pdfmake/pdfmake.min.js') }}"></script>
+<script src="{{ asset('backend/assets/plugins/datatable/pdfmake/vfs_fonts.js') }}"></script>
+<script src="{{ asset('backend/assets/plugins/datatable/js/butsns.html5.min.js') }}"></script>
+<script src="{{ asset('backend/assets/plugins/datatable/js/buttons.print.min.js') }}"></script>
+<script src="{{ asset('backend/assets/plugins/datatable/js/buttons.colVis.min.js') }}"></script>
+<script src="{{ asset('backend/assets/plugins/datatable/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('backend/assets/plugins/datatable/responsive.bootstrap5.min.js') }}"></script>
+<script src="{{ asset('backend/assets/js/table-data.js') }}"></script>
 
 {{-- APEXCHART JS --}}
 <script src="{{ asset('backend/js/apexcharts.js') }}"></script>
@@ -56,26 +56,26 @@
 <script src="{{ asset('backend/plugins/circle-progress/circle-progress.min.js') }}"></script>
 
 {{-- INDEX JS --}}
-<script src="{{ asset('backend/js/index1.js') }}"></script>
-<script src="{{ asset('backend/js/index.js') }}"></script>
+<script src="{{ asset('backend/assets/js/index1.js') }}"></script>
+<script src="{{ asset('backend/assets/js/index.js') }}"></script>
 
 {{-- Reply JS --}}
-<script src="{{ asset('backend/js/reply.js') }}"></script>
+<script src="{{ asset('backend/assets/js/reply.js') }}"></script>
 
 {{-- COLOR THEME JS --}}
-<script src="{{ asset('backend/js/themeColors.js') }}"></script>
+<script src="{{ asset('backend/assets/js/themeColors.js') }}"></script>
 
 {{-- CUSTOM JS --}}
-<script src="{{ asset('backend/js/custom.js') }}"></script>
+<script src="{{ asset('backend/assets/js/custom.js') }}"></script>
 
 {{-- SWITCHER JS --}}
-<script src="{{ asset('backend/switcher/js/switcher.js') }}"></script>
+<script src="{{ asset('backend/assets/switcher/js/switcher.js') }}"></script>
 
 {{-- SweetAlert2 JS --}}
-<script src="{{ asset('backend/js/sweetalert2@11.js') }}"></script>
+<script src="{{ asset('backend/assets/js/sweetalert2@11.js') }}"></script>
 
 {{-- Ckeditor.js --}}
-<script src="{{ asset('backend/custom_downloaded_file/ckeditor.js') }}"></script>
+<script src="{{ asset('backend/assets/custom_downloaded_file/ckeditor.js') }}"></script>
 
 {{-- toastr start --}}
 <script>
